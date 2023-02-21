0b135b49975829c63476ef4a641f27d8e242283c
Andrea Bustos <andreastefbustos@gmail.com>
2023-02-21 19:06:44 -0300

    Manejo del Estado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 // import './App.css';
 
-import React from "react";
+import React, { useState } from 'react';
 import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
-const todos = [
-  {text: 'Cortar cebollas', completed: false},
+const defaultTodos = [
+  {text: 'Cortar cebollas', completed: true},
   {text: 'Cortar tomates', completed: false},
   {text: 'Cortar paltas', completed: false}
 ]
 
 
 function App() {
+  const [todos, setTodos] = useState(defaultTodos)
+  const [searchValue, setSearchValue] = useState('')
+  const completedTodos = todos.filter(todo => !!todo.completed).length
+  const totalTodos = todos.length
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >=1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    })
+  }
+  
   return (
     <React.Fragment>
-      <TodoCounter />
-      <input placeholder="Cebolla"/> 
+      <TodoCounter
+      total={totalTodos}
+      completed={completedTodos}
+      />
+      
+      <TodoSearch
+      searchValue={searchValue}
+      setSearchValue={setSearchValue} 
+      />
       
       <TodoList>
-        {todos.map(todo => (
-            <TodoItem key={todo.text} text={todo.text}/>
+        {searchedTodos.map(todo => (
+            <TodoItem 
+              key={todo.text} 
+              text={todo.text}
+              completed={todo.completed}
+            />
           ))}
       </TodoList>
       
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,32 @@
 import React from "react";
+import './TodoItem.css';
 
 function TodoItem(props){
+    const onCompleted = () => {
+        alert('Ya completaste la tarea ' + props.text)
+    }
+
+    const onDelete = () => {
+        alert('Eliminar tarea ' + props.text)
+    }
     return (
-        <li>
-            <span>C</span>
-            <p>{props.text}</p>
-            <span>X</span>
+        <li className="TodoItem">
+            <span 
+            className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+            onClick= {onCompleted}>
+                √
+            </span>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+                {props.text}
+            </p>
+            <span 
+            className="Icon Icon-delete"
+            onClick={onDelete}>
+                X
+            </span>
         </li>
     );
 }
 
 export { TodoItem };
+
