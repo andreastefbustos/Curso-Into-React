baa86ae03b4b4d4a1b494380af96b85e60cdd93b
Andrea Bustos <andreastefbustos@gmail.com>
2023-04-03 18:38:20 -0400

    Using Icon React

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,22 +1,35 @@
 import React from "react";
-import { CompleteIcon } from "../TodoIcon/CompleteIcon";
-import { DeleteIcon } from "../TodoIcon/DeleteIcon";
+// import { CompleteIcon } from "../TodoIcon/CompleteIcon";
+// import { DeleteIcon } from "../TodoIcon/DeleteIcon";
+import { HiCheckCircle } from "react-icons/hi";
+import { MdOutlineDeleteForever } from "react-icons/md"
 import './TodoItem.css';
 
 function TodoItem(props){
    
     return (
         <li className="TodoItem">
-            <CompleteIcon
+            {/* <CompleteIcon
             completed={props.completed}
             onComplete={props.onComplete}
-            />
+            /> */}
+            <span 
+            className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+            onClick= {props.onComplete}>
+                <HiCheckCircle />
+            </span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
-            <DeleteIcon
+            {/* <DeleteIcon
             onDelete={props.onDelete}
-            />
+            /> */}
+            <span 
+            className="Icon Icon-delete"
+            onClick={props.onDelete}>
+                <MdOutlineDeleteForever />
+            </span>
+
         </li>
     );
 }
